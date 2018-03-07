9de28892fbc283960c106bebc95d7b49f593d300
Conner Jensen <connerj70@gmail.com>
2018-03-07 14:42:23 -0700

    working

--- db/create_response.sql
+++ db/create_response.sql
@@ -1,2 +1,4 @@
-insert into ann_responses(response, letter_id)
-values($1, $2);
+INSERT INTO ann_responses (response, letter_id) 
+VALUES ($1, $2)
+ON CONFLICT (letter_id) DO UPDATE 
+  SET response = excluded.response;
